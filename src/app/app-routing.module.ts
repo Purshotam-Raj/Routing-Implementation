4b1f5bb6ce3e0f1f0211423338b39660d59c926d
Purshotam Raj <purshotam.raj@fretron.com>
2021-03-20 14:32:46 +0530

    Routing Parametrised wildcard implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderViewComponent } from './order-view/order-view.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductIdComponent } from './product-id/product-id.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductsComponent } from './products/products.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'product-edit', pathMatch:'full'},
+  { path:'products', component: ProductsComponent },
+  { path:'product-view', component: ProductViewComponent },
+  { path:'product-edit', component: ProductEditComponent },
+  { path:'product/10', component: ProductIdComponent }, // this value should be dynamic
+  { path:'order/:id/:id2', component: OrderViewComponent },
+  { path:'search', component: SearchComponent },
+  {path:'**', component: PageNotFoundComponent}
+];
+
+// Product value should be dynamic ;
+// Redirecting the routes
+// able to implement wild card route
+// default route
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { PathLocationStrategy, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductIdComponent } from './product-id/product-id.component';
+import { OrderViewComponent } from './order-view/order-view.component';
+import { SearchComponent } from './search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductViewComponent,
+    ProductEditComponent,
+    ProductIdComponent,
+    OrderViewComponent,
+    SearchComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: PathLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
